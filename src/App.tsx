2a2721636e8365c3bdcba9aa0ad05eb46b933415
Christopher Forti <christopher.forti.79@gmail.com>
2024-12-31 21:33:22 -0500

    Startyed working mainpage content.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/header";
+import { Main } from "./components/main";
 
 function App() {
   return (
     <div className="flex min-h-screen min-w-96 bg-slate-600 pt-4">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/main.tsx
@@ -0,0 +1,13 @@
+export function Main() {
+  return (
+    <div className="container mx-auto mt-10">
+      <div className="whitespace-normal text-white">
+        <h1 className="mb-6 text-3xl font-bold">
+          Hey everyone! It's 2025 and we still don't know if there is aliens
+          living among us, or do we? Maybe the person writing this is an alien.
+          You will never know.
+        </h1>
+      </div>
+    </div>
+  );
+}
