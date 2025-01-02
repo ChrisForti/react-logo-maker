1087f3acbf72a5eb95c67f72e727e77da359b033
Christopher Forti <christopher.forti.79@gmail.com>
2025-01-01 19:04:51 -0500

    Fixed error in vite.confdig.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ const base = process.env.NODE_ENV === "production" ? "/react-logo-maker/" : "/";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base,
+  base: base,
 });
