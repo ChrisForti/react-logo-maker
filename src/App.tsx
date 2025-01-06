a1ad702d5dfdcd9b9dbaf3879d25e8ebb3c521ff
Christopher Forti <christopher.forti.79@gmail.com>
2025-01-05 21:24:40 -0500

    Finished inputting from class code plus notes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
+import { useState } from "react";
+// import { Counter } from "./components/Counter";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 
 function App() {
+  // const [number, setNumber] = useState(0);
+  const [brand, setBrand] = useState(""); // create state
+
+  function handleSubmit() {
+    // const data = {
+    //   brand,
+    //   subBrand,
+    // };
+    console.log("Clicked!");
+  }
+
   return (
-    <div className="flex min-h-screen min-w-96 bg-slate-600 pt-4">
-      <Header />
+    <div className="min-w96 flex min-h-screen flex-col gap-4 bg-slate-600 text-center">
+      <Header num={30000} />
+      <input // use the state
+        type="text"
+        value={brand}
+        onChange={(event) => {
+          setBrand(event.target.value);
+        }}
+        className="mx-auto w-96 rounded"
+      />{" "}
+      <button
+        className="rounded bg-slate-400 px-8 py-4 text-lg"
+        onClick={handleSubmit}
+      >
+        submit
+      </button>
+      <div>{/* <Counter num={number} setter={setNumber} /> */}</div>
       <Main />
     </div>
   );
 }
 
 export default App;
+
+// Every piece of state, and input for all things needed in a logo maker in the App.tsx
+// text color
+// padding
+// shape
+// margin
+//
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,18 @@
-export function Header() {
+type HeaderProps = {
+  num: number;
+};
+
+export function Header(props: HeaderProps) {
   return (
     <div>
-      <header className="pl-5 text-7xl text-white">Testing tailwind</header>
+      <header className="items flex items-center justify-between bg-slate-700 p-4 text-white">
+        <h1 className="text-7xl">Logo maker {props.num}</h1>
+        <nav>
+          <a className="text-2xl" href="https://github.com/ChrisForti">
+            Github
+          </a>
+        </nav>
+      </header>
     </div>
   );
 }
