15712c8e2b89b1ab6f81077a21510dd27d8abf41
Christopher Forti <christopher.forti.79@gmail.com>
2025-01-28 19:12:08 -0500

    Removed StringInputProps from imports in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Header } from "./components/Header";
 import EditPane from "./components/EditPane";
-import { StringInputProps } from "./components/Input";
 
 function App() {
   const [brand, setBrand] = useState(""); // create the state
